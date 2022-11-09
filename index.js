99e1607dcbb08b62cc3aa43bbdb3770a7b433bd2
Tim Gabrikowski <timgabrikowski@outlook.de>
2022-11-09 21:47:40 +0100

    add files via upload

---
+++ index.js
@@ -0,0 +1,132 @@
+function coinflip() {
+    return Math.random() < 0.5;
+}
+
+function generatePair(){
+
+    celA = {
+        color: coinflip() ? "G":"g",
+        form: coinflip() ? "R":"k"
+    }
+    celB = {
+        color: coinflip() ? "G":"g",
+        form: coinflip() ? "R":"k"
+    }
+
+    return {
+        a: celA,
+        b: celB
+    }
+
+}
+
+function calcGens(celA, celB){
+    var out = {
+        colorA: "",
+        colorB: "",
+        formA: "",
+        formB: ""
+    }
+
+    // COLORS
+    // GG
+    if(celA.color == "G" && celB.color == "G") {
+        out.colorA = "G";
+        out.colorB = "G";
+    }
+    // Gg | gG
+    if(celA.color != celB.color){
+        out.colorA = "G";
+        out.colorB = "g";
+    }
+    // gg
+    if(celA.color == "g" && celB.color == "g") {
+        out.colorA = "g";
+        out.colorB = "g";
+    }
+
+    // FORMS
+    // RR
+    if(celA.form == "R" && celB.form == "R") {
+        out.formA = "R";
+        out.formB = "R";
+    }
+    // Rk | kR
+    if(celA.form != celB.form){
+        out.formA = "R";
+        out.formB = "k";
+    }
+    // kk
+    if(celA.form == "k" && celB.form == "k") {
+        out.formA = "k";
+        out.formB = "k";
+    }
+    return out;
+}
+
+function genString(gens){
+    return gens.colorA + gens.colorB + gens.formA + gens.formB;
+}
+
+function calcPhen(gens){
+    var color = gens.colorA == "G" ? "gelb":"grün";
+    var form = gens.formA == "R" ? "rund":"kantig";
+
+    return color + "/" + form;
+}
+
+var stats = {
+    gelb_rund: 0,
+    gelb_kantig: 0,
+    gruen_rund: 0,
+    gruen_kantig: 0
+}
+
+function countStats(childs) {
+    for(var child of childs){
+        switch (child.phen){
+            case "gelb/rund":
+                stats.gelb_rund++;
+                break;
+            case "gelb/kantig":
+                stats.gelb_kantig++;
+                break;
+            case "grün/rund":
+                stats.gruen_rund++;
+                break;
+            case "grün/kantig":
+                stats.gruen_kantig++;
+                break;
+        }
+    }
+}
+
+function genChild(){
+    cellPair = generatePair();
+    childGens = calcGens(cellPair.a, cellPair.b);
+
+    childString = genString(childGens);
+    childPhen = calcPhen(childGens);
+
+    var child = {pair: cellPair, gens: childGens, string: childString, phen: childPhen}
+    return child;
+}
+
+var ITERATIONS = 1048576;
+
+var estimations = {
+    gelb_rund: ITERATIONS * 9/16,
+    gelb_kantig: ITERATIONS * 3/16,
+    gruen_rund: ITERATIONS * 3/16,
+    gruen_kantig: ITERATIONS * 1/16
+}
+
+var genList = [];
+
+for (var i = 0; i < ITERATIONS; i++) {
+    genList.push(genChild());
+}
+countStats(genList);
+console.log("Iterations:", ITERATIONS);
+console.log("Estimation:", estimations);
+console.log("Result    :", stats);
